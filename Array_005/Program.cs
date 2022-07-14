1051f8b989f788c707d61fa64cbb2a62ae6023d1
Kirill Romanov <freibird22@gmail.com>
2022-07-14 14:40:48 +0300

    Filling array with Random elements.

---
+++ Array_005/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection) //void "not returning value" 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) // void - пустота
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int[] array = new int[10]; //make new array with 10 elements.
+
+FillArray(array);
+PrintArray(array);
