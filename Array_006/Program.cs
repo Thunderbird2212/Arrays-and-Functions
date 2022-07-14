91683276928712852fdb7146724a17228618c009
Kirill Romanov <freibird22@gmail.com>
2022-07-14 15:20:16 +0300

    Finding element in array with method.

---
+++ Array_006/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection) //void "not returning value" 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col) // void - пустота
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //-1 чтобы исключить элемент которого нет в массиве.
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return (position);
+}
+
+int[] array = new int[10]; //make new array with 10 elements.
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 664);
+Console.WriteLine(pos);
