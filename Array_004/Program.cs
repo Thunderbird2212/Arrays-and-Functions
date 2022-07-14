4038340ffce5b797578be346dbfeb7d9d3a2cac5
Kirill Romanov <freibird22@gmail.com>
2022-07-14 13:45:59 +0300

    Find element in array added.

---
+++ Array_004/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 34, 12, 3, 34, 5, 16, 7, 88, 91 };
+int n = array.Length;
+int find = 34;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //find first similar element 
+    }
+    //index = index + 1;
+    index++;
+}
